fca11adc4542acfe9387ad1e0ec9e05cb025c08e
Ahmed <dulaimi89@gmail.com>
2018-11-02 13:15:36 +0000

    styled the page

--- code.js
+++ code.js
@@ -1,32 +1,37 @@
-
 fetchData();
-function expand(id){
-   const section = document.querySelector(`#${id}`)
-   section.classList.toggle("active")
-   section.classList.toggle("collapse")
+function expand(id) {
+  const section = document.querySelector(`#section${id}`);
+  const content = document.querySelector(`#content${id}`);
+  section.classList.toggle("active");
+  section.classList.toggle("collapsible");
+  content.classList.toggle("hidden");
+  content.classList.toggle("section__content");
 }
 
-
-function fetchData(){
-fetch('https://design.propcom.co.uk/buildtest/accordion-data.json')
-.then(response=>response.json())
-.then(json=>displayData(json.blocks))
-.catch(err => console.log(err))
+function fetchData() {
+  fetch("https://design.propcom.co.uk/buildtest/accordion-data.json")
+    .then(response => response.json())
+    .then(json => displayData(json.blocks))
+    .catch(err => console.log(err));
 }
 
-function displayData(json){
-const data = json.map((section,index)=>{
-    return(
-    `
-    <div id="section${index+1}" class="collapse" onclick="expand('section${index+1}')">
+function displayData(json) {
+  const data = json
+    .map((section, index) => {
+      console.log(section.content);
+      return `
+    <div id="section${index + 1}"
+     class="collapsible section" 
+     onclick="expand('${index + 1}')">
     ${section.heading}
-    <p> ${section.content} </p>
+    <div id="content${index + 1}"
+    class="hidden" >
+     ${section.content} </div>
     </div>
-    `
-    );
-})
+    `;
+    })
+    .join("");
 
-const sections = document.querySelector('.sections')
-sections.innerHTML = data;
-
+  const sections = document.querySelector(".sections");
+  sections.innerHTML = data;
 }
